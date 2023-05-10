ac06bee1d83cb4d21acbb5cb8242e6fd2540d4c4
Mark Lauter <mark@marklauter.com>
2023-05-10 12:12:58 -0400

    cleaning up xunit sample

--- LogSample.Tests.XUnit/SampleServiceTests.cs
+++ LogSample.Tests.XUnit/SampleServiceTests.cs
@@ -18,12 +18,15 @@ namespace LogSample.Tests.XUnit
         [Fact]
         public void Succeed_Succeeds()
         {
-            var id = Guid.NewGuid();
-            this.sampleService.Succeed(new SampleData(id, nameof(Succeed_Succeeds), DateTime.UtcNow));
+            var sampleData = new SampleData(
+                Guid.NewGuid(),
+                nameof(Succeed_Succeeds),
+                DateTime.UtcNow);
+            this.sampleService.Succeed(sampleData);
 
             var stdout = ((Xunit.Sdk.TestOutputHelper)this.output).Output;
             Assert.Contains("SampleData", stdout);
-            Assert.Contains(id.ToString(), stdout);
+            Assert.Contains(sampleData.Id.ToString(), stdout);
         }
     }
 }
--- LogSample.Tests.XUnit/Startup.cs
+++ LogSample.Tests.XUnit/Startup.cs
@@ -1,29 +1,31 @@
-﻿using LogSample.Library;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Xunit.DependencyInjection;
 using Xunit.DependencyInjection.Logging;
+
+#pragma warning disable CA1822 // Mark members as static
+#pragma warning disable IDISP004 // Don't ignore created IDisposable
 
 namespace LogSample.Tests.XUnit
 {
     public sealed class Startup
     {
-#pragma warning disable CA1822 // Mark members as static
         public void ConfigureServices(IServiceCollection services)
-#pragma warning restore CA1822 // Mark members as static
         {
             _ = services
                 .AddLogging()
                 .AddSampleService();
         }
 
-#pragma warning disable CA1822 // Mark members as static
         public void Configure(ILoggerFactory loggerFactory, ITestOutputHelperAccessor accessor)
         {
-#pragma warning disable IDISP004 // Don't ignore created IDisposable
-            loggerFactory.AddProvider(new XunitTestOutputLoggerProvider(accessor, (source, ll) => ll >= LogLevel.Trace));
-#pragma warning restore IDISP004 // Don't ignore created IDisposable
+            loggerFactory.AddProvider(
+                new XunitTestOutputLoggerProvider(
+                    accessor,
+                    (source, ll) => ll >= LogLevel.Trace));
         }
-#pragma warning restore CA1822 // Mark members as static
     }
 }
+
+#pragma warning restore IDISP004 // Don't ignore created IDisposable
+#pragma warning restore CA1822 // Mark members as static
